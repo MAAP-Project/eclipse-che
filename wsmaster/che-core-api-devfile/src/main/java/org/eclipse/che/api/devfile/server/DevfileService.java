834d1de1c34487f35b78a31a3e96e9eed2b89fa6
bsatoriu <brian.p.satorius@jpl.nasa.gov>
2021-03-28 16:52:59 -0700

    Update DevfileService.java

--- wsmaster/che-core-api-devfile/src/main/java/org/eclipse/che/api/devfile/server/DevfileService.java
+++ wsmaster/che-core-api-devfile/src/main/java/org/eclipse/che/api/devfile/server/DevfileService.java
@@ -100,7 +100,7 @@ public class DevfileService extends Service {
   @Consumes({APPLICATION_JSON, "text/yaml", "text/x-yaml"})
   @Produces(APPLICATION_JSON)
   @ApiOperation(
-      value = "Creates a new persistent Devfile from yaml representation",
+      value = "Creates a new persistent Devfile from yaml representation..",
       consumes = "application/json, text/yaml, text/x-yaml",
       produces = APPLICATION_JSON,
       nickname = "createFromDevfileYaml",
